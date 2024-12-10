5a96682c1df3e2467a92c004e94b181887caa707
Juan Pablo Toledo <toledo.pablo87@gmail.com>
2024-12-10 20:18:26 -0300

    se agrego Ejercicio 3  del tp

---
+++ JS/Ejercicio3.js
@@ -0,0 +1,34 @@
+let sumas = [];
+
+for (let i = 0; i < 50; i++) {
+    let dado1 = Math.floor(Math.random() *6) + 1;
+    let dado2 = Math.floor(Math.random() *6) + 1;
+
+    sumas.push(dado1+dado2)
+    
+}
+console.log(sumas);
+
+sumas.forEach((suma, index)=>{
+    console.log(`Tirada N° ${index + 1 } // Suma:`);
+    
+})
+
+let SumasUnicas = [];
+let ocurrencias = [];
+
+for (let i = 0; i < sumas.length; i++) {
+    let suma = sumas[i];
+
+    let index = SumasUnicas.indexOf(suma)
+
+    if (index === -1) {
+        SumasUnicas.push(suma);
+        ocurrencias.push(1);
+    } else {
+        ocurrencias[index]++
+    }
+    
+}
+document.write(`<br>Sumas Unicas: ${SumasUnicas}`)
+document.write(`<br>Sumas por ocurrencias: ${ocurrencias}`)
