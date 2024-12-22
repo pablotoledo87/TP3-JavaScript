59a2ad877666d485e79b064c6fbebf1033f81494
Juan Pablo Toledo <toledo.pablo87@gmail.com>
2024-12-21 21:14:19 -0300

    se agrego ejercicio 1 del tp

---
+++ JS/Ejercicio1.js
@@ -0,0 +1,7 @@
+let meses = ["Enero ","Febrero","Marzo" , "Abril", "mayo","Junio","Julio","Agosto","Setiembre","Octubre","Noviembre","Diciembre"]
+
+
+for (let i = 0; i < meses.length; i++) {
+    document.write(`<li>Los meses del año son: ${meses[i]}</li>`)
+    
+}
