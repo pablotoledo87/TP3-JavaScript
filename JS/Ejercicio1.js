33dc8093ed94a845e50a9290234426976b509753
Juan Pablo Toledo <toledo.pablo87@gmail.com>
2024-12-23 10:28:54 -0300

    se actualizo ejercicio 1

--- JS/Ejercicio1.js
+++ JS/Ejercicio1.js
@@ -1,7 +1,7 @@
 let meses = ["Enero ","Febrero","Marzo" , "Abril", "mayo","Junio","Julio","Agosto","Setiembre","Octubre","Noviembre","Diciembre"]
 
-
+document.write(`<h1>Lista de meses</h1>`)
 for (let i = 0; i < meses.length; i++) {
-    document.write(`<li>Los meses del año son: ${meses[i]}</li>`)
+    document.write(`<li>${meses[i]}</li>`)
     
 }
